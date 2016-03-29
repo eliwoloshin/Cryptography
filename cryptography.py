f2c60fe7c1c16c2bb7ab25149da38441e3d72ef2
eliwoloshin <eliwoloshin@hanovernorwichschools.org>
2016-03-29 14:42:25 -0400

    Updated from Brython Server: 3/29/2016 2:42:25 PM

--- cryptography.py
+++ cryptography.py
@@ -1,1 +1,26 @@
-print("Hello, world.")
+"""
+cryptography.py
+Author: Eli Woloshin
+Credit: none
+Assignment:
+Write and submit a program that encrypts and decrypts user data.
+See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
+"""
+associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+z = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+list = ["e", d"]
+if z
+    print("Did not understand command, try again.")
+
+Enter e to encrypt, d to decrypt, or q to quit: z
+Did not understand command, try again.
+Enter e to encrypt, d to decrypt, or q to quit: e
+Message: Two plus two = Five
+Key: Lorem ipsum
++KF;B(CH=NIZ}m;R\Dt
+Enter e to encrypt, d to decrypt, or q to quit: d
+Message: +KF;B(CH=NIZ}m;R\Dt
+Key: Lorem ipsum
+Two plus two = Five
+Enter e to encrypt, d to decrypt, or q to quit: q
+Goodbye!
