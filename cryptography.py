b82d01697b46d9285c5a3d9e2c02a097dd57cbe5
eliwoloshin <eliwoloshin@hanovernorwichschools.org>
2016-04-07 14:28:42 -0400

    Updated from Brython Server: 4/7/2016 2:28:43 PM

--- cryptography.py
+++ cryptography.py
@@ -7,26 +7,35 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
-z = input("Enter e to encrypt, d to decrypt, or q to quit: ")
-message = input("Message: ")
-key = input("Key: ")
 
 
 while key.count <= message.count:
     print()
 
-if z == "e":
-    str.split(message)
-    
-    if z == "d":
+z=""
+while z !="q":
+    z = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    message = input("Message: ")
+    key = input("Key: ")
+    a=[]
+    for c in z:
+        a.append(c)
+    b=[]
+    for c in key:
+        b.append(c)
+    if z == "e":
+        tuples = zip(a, b)
+        print(list(tuples))
+    elif z == "d":
         print()
-
-if z != "q" and z != "e" and z != "d":
-    print("Did not understand command, try again.")
-if z == "q":
-    print("Goodbye!")
+    if z != "q" and z != "e" and z != "d":
+        print("Did not understand command, try again.")
+    if z == "q":
+        print("Goodbye!")
 
 
 
 associations.find("e")
 associations[index]
+
+
