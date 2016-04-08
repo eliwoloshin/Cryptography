1b28b284a4499609eae5039bfbd24cb3f1927aa4
eliwoloshin <eliwoloshin@hanovernorwichschools.org>
2016-04-08 14:19:14 -0400

    Updated from Brython Server: 4/8/2016 2:19:14 PM

--- cryptography.py
+++ cryptography.py
@@ -6,7 +6,7 @@ Assignment:
 Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
-associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"*2
 
 
 #while key.count <= message.count:
